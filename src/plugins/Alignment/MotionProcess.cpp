786070018ef4c206784234dad9ae5f37d3282aa1
zheD <zhe.dong@mloptic.co>
2026-04-02 10:54:15 +0800

    add init DUT

--- src/plugins/Alignment/MotionProcess.cpp
+++ src/plugins/Alignment/MotionProcess.cpp
@@ -90,7 +90,7 @@ namespace AAProcess
 				return msg;
 			}
 		}
-		
+
 		// 2D tilt station 
 		//if (!Motion2DModel::getInstance(ACS2DCameraTilt)->Motion2DisConnected())
 		{
@@ -158,7 +158,7 @@ namespace AAProcess
 		std::string msg;
 
 		// Projector 2D tilt
-		
+
 		//if (!Motion2DModel::getInstance(ACS2DPro)->GetIsConnected())
 		{
 			QString ipEtc = ConfigItem::instance()->getMotion2DIpAndOther(ACS2DPro);
@@ -244,37 +244,31 @@ namespace AAProcess
 		return "";
 	}
 
-	std::string MotionProcess::LoadDUT()
+	std::string MotionProcess::InitSafePosAdjust() 
 	{
-		PrintLog(LogType::Normal, "[LoadDUT Start]");
+		PrintLog(LogType::Normal, "[InitSafePosAdjust Start]");
 		if (currentDutName == "")
 		{
 			return "Pleasure check DUT Type!";
 		}
-		std::string msg = CheckModuleConnectStatus(ModuleName::DutModuleXYZ, ModuleName::ImagingModuleXYZ);
-		if (msg != "")
-			return msg;
+		std::string msg;
+
+		//TODO: pre judge
 
 		//----------- adjust safe ---------------//
-
 		msg = LoadSLB();
 		if (msg != "")
 			return msg;
 
 		//----------- adjust DUT tiptilt ---------------//
+		msg = CheckModuleConnectStatus(ModuleName::ImagingModuleDxDy, ModuleName::ProjectionDxDy);
+		if (msg != "")
+			return msg;
 
 		double projectionTiptiltX = m_processConfigInfo.offsetRoatate.projectionTiptilt[currentDutName].x;
 		double projectionTiptiltY = m_processConfigInfo.offsetRoatate.projectionTiptilt[currentDutName].y;
 		Motion2DModel::getInstance(motion2DType::ACS2DPro)->Motion2DMoveAbsAsync(projectionTiptiltX, projectionTiptiltY);
-		/*while (CheckModuleIsMoving(ModuleName::ProjectionDxDy))
-		{
-			QCoreApplication::processEvents();
-			_sleep(100);
-		}
-		PrintLog(LogType::Normal, "projection module adjust tiptilt end.");
-		PrintModulePosition(ModuleName::ProjectionDxDy);*/
 
-		//adjust imaging module tiptilt
 		double imgingTiptiltX = m_processConfigInfo.offsetRoatate.imagingTiptilt[currentDutName].x;
 		double imgingTiptiltY = m_processConfigInfo.offsetRoatate.imagingTiptilt[currentDutName].y;
 		Motion2DModel::getInstance(motion2DType::ACS2DCameraTilt)->Motion2DMoveAbsAsync(imgingTiptiltX, imgingTiptiltY);
@@ -294,6 +288,60 @@ namespace AAProcess
 		PrintLog(LogType::Normal, "imaging module adjust tiptilt end.");
 
 		PrintModulePosition(ModuleName::ImagingModuleDxDy, ModuleName::ProjectionDxDy);
+
+		return "";	
+	}
+
+	std::string MotionProcess::LoadDUT()
+	{
+		PrintLog(LogType::Normal, "[LoadDUT Start]");
+		if (currentDutName == "")
+		{
+			return "Pleasure check DUT Type!";
+		}
+		std::string msg = CheckModuleConnectStatus(ModuleName::DutModuleXYZ, ModuleName::ImagingModuleXYZ);
+		if (msg != "")
+			return msg;
+
+		////----------- adjust safe ---------------//
+
+		//msg = LoadSLB();
+		//if (msg != "")
+		//	return msg;
+
+		////----------- adjust DUT tiptilt ---------------//
+
+		//double projectionTiptiltX = m_processConfigInfo.offsetRoatate.projectionTiptilt[currentDutName].x;
+		//double projectionTiptiltY = m_processConfigInfo.offsetRoatate.projectionTiptilt[currentDutName].y;
+		//Motion2DModel::getInstance(motion2DType::ACS2DPro)->Motion2DMoveAbsAsync(projectionTiptiltX, projectionTiptiltY);
+		///*while (CheckModuleIsMoving(ModuleName::ProjectionDxDy))
+		//{
+		//	QCoreApplication::processEvents();
+		//	_sleep(100);
+		//}
+		//PrintLog(LogType::Normal, "projection module adjust tiptilt end.");
+		//PrintModulePosition(ModuleName::ProjectionDxDy);*/
+
+		////adjust imaging module tiptilt
+		//double imgingTiptiltX = m_processConfigInfo.offsetRoatate.imagingTiptilt[currentDutName].x;
+		//double imgingTiptiltY = m_processConfigInfo.offsetRoatate.imagingTiptilt[currentDutName].y;
+		//Motion2DModel::getInstance(motion2DType::ACS2DCameraTilt)->Motion2DMoveAbsAsync(imgingTiptiltX, imgingTiptiltY);
+
+		//while (CheckModuleIsMoving(ModuleName::ImagingModuleDxDy, ModuleName::ProjectionDxDy))
+		//{
+		//	if (m_isStopTreeSystem.load())
+		//	{
+		//		StopModuleMove(ModuleName::ImagingModuleDxDy, ModuleName::ProjectionDxDy);
+		//		m_isStopTreeSystem.store(false);
+		//		return "Operation is force stopped by user.";
+		//	}
+		//	QCoreApplication::processEvents();
+		//	_sleep(100);
+		//}
+		//PrintLog(LogType::Normal, "projection module adjust tiptilt end.");
+		//PrintLog(LogType::Normal, "imaging module adjust tiptilt end.");
+
+		//PrintModulePosition(ModuleName::ImagingModuleDxDy, ModuleName::ProjectionDxDy);
 
 
 		//----------- adjust Fixpos ---------------//
